ae5fd45197e83534a77b3072b81d92891ac7238c
khoirulhudaadev <muhammadkhoirulhuda111@gmail.com>
2026-01-23 10:19:39 +0700

    All features (done)

--- frontend/src/components/auth/SignInForm.tsx
+++ frontend/src/components/auth/SignInForm.tsx
@@ -81,7 +81,7 @@ export default function SignInForm() {
         <div className="relative top-[-10px]">
           <div className="mb-5 sm:mb-8">
             <h1 className="mb-2 font-semibold text-gray-800 text-title-sm dark:text-white/90 sm:text-title-md">
-              Masuk ke
+              Masuk
             </h1>
             <p className="text-sm text-gray-500 dark:text-gray-400">
               Masukkan email dan kata sandi untuk mengakses
